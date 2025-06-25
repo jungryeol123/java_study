dc5d135d6554caf38352452123e9f5602e57fa23
yoon <46823971@naver.com>
2025-06-25 18:23:05 +0900

    .md modified commit

--- src/chapter03/Test.java
+++ src/chapter03/Test.java
@@ -6,8 +6,11 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.print("메모할 내용을 적으세요 : ");
 		
-		Scanner write = new Scanner(System.in);
-		String memo = write.next();
+		Scanner scan;
+		scan = new Scanner(System.in);
+		
+		String memo = scan.next();
+		
 		System.out.print("메모 내용 : "+memo);
 	}
 }
