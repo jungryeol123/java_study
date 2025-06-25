b593b9f1ceace354bb9849c1ce602da80a6d2cff
yoon <46823971@naver.com>
2025-06-25 12:00:28 +0900

    .md modified commit

--- src/chapter02/Hello.java
+++ src/chapter02/Hello.java
@@ -3,7 +3,7 @@ package chapter02;
 public class Hello {
 		//main method
 		public static void main(String[] args) {
-			System.out.print("Welcome to JAVA!!");
+			System.out.println("hi");
 			System.out.println("Hello~");
 			
 			
---
+++ src/chapter03/Variable.java
@@ -0,0 +1,22 @@
+package chapter03;
+
+public class Variable {
+		public static void main(String[] args) {
+		//	기본 자료형 변수 정의
+			int age = 10;
+			double aged = 10.0;
+			boolean flag = true;    //true(1), false(2)
+			char name = '홍';
+			
+			
+			
+			System.out.println(age);
+			System.out.println(aged);
+			System.out.println(flag); 
+			System.out.println(name);
+			
+			
+					
+					
+	}
+}
